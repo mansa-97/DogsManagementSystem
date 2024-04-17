07195aa6515416cb02f200026acda0cf5e405f51
mansa-97 <S567011@nwmissouri.edu>
2024-04-17 14:53:20 -0500

    All files

---
+++ src/main/java/com/fellahClasses/DMS/DogManagementSystemApplication.java
@@ -0,0 +1,19 @@
+package com.fellahClasses.DMS;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jayanth Anumula
+ * Due: 04/5/24
+ */
+
+@SpringBootApplication
+public class DogManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogManagementSystemApplication.class, args);
+	}
+
+}
